bcf3b81c6a8bff14b2ee69bef5835391765f702a
Jackie <104796329+thejackiewilson@users.noreply.github.com>
2023-01-23 09:27:45 -0500

    Update strategymap.py

--- strategymap.py
+++ strategymap.py
@@ -1,25 +1,26 @@
 import matplotlib.pyplot as plt
 
 # Define the strategic objectives and their relationships
-strategic_objectives_trader_joes = {
-    'Financial': ['Revenue growth', 'Profitability'],
-    'Customer': ['Customer satisfaction', 'Market share'],
-    'Internal': ['Efficiency', 'Innovation'],
-    'Team Growth': ['Employee satisfaction', 'Talent development']
+strategic_objectives = {
+    'Customer': ['Customer satisfaction', 'Market share', 'Customer retention'],
+    'Financial': ['Revenue growth', 'Profitability', 'Cost management'],
+    'Internal': ['Product quality', 'Efficiency', 'Partnerships with local suppliers'],
+    'Learning & Growth': ['Employee satisfaction', 'Talent development', 'Technology integration'],
+    'Customer experience': ['Unique shopping experience', 'Friendly customer service', 'Engaging in-store promotions']
 }
 
 # Create the strategy map
 fig, ax = plt.subplots()
-ax.set_title('Strategy Map of Trader Joes')
+ax.set_title('Trader Joe\'s Strategy Map')
 
 # Draw the strategic objectives as boxes
-for i, (key, value) in enumerate(strategic_objectives_trader_joes.items()):
+for i, (key, value) in enumerate(strategic_objectives.items()):
     ax.text(0.5, i, key, ha='center')
     for j, obj in enumerate(value):
         ax.text(1, i-0.25+j*0.25, obj, ha='left')
 
 # Draw lines connecting the strategic objectives
-for i, (key, value) in enumerate(strategic_objectives_trader_joes.items()):
+for i, (key, value) in enumerate(strategic_objectives.items()):
     for obj in value:
         ax.plot([0.5, 1], [i-0.5, i-0.25+value.index(obj)*0.25], '-o', c='black')
 
@@ -27,5 +28,5 @@ for i, (key, value) in enumerate(strategic_objectives_trader_joes.items()):
 ax.set_xticks([])
 ax.set_yticks([])
 ax.set_xlim([0, 2])
-ax.set_ylim([-1, len(strategic_objectives_trader_joes)])
+ax.set_ylim([-1, len(strategic_objectives)])
 plt.show()
